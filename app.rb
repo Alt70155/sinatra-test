2e4c6de72562046ab69f9e1b1e377ac85d9dcde2
chika <chika1086@gmail.com>
2019-05-20 13:44:34 +0900

    add File posting process

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'slim'
 require 'sinatra/reloader'
 require 'active_record'
+# enable :sessions　これだとダメらしい
+use Rack::Session::Cookie
 
 # database.ymlを読み込んでくれえ
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
@@ -24,15 +26,24 @@ end
 
 get '/articles/:id' do
   @post = Post.find(params[:id])
-  @category = Category.where(cate_id: 1)
+  @category = Category.where(cate_id: @post.cate_id)
+  @file_name = session[:file_name]
   slim :articles
 end
 
 post '/article_post' do
   post_data = Post.create(
-    title: params[:title],
-    body:  params[:body],
+    title:   params[:title],
+    body:    params[:body],
     cate_id: params[:cate_id]
   )
+  # file受け取り
+  @file_name = params[:file][:filename]
+  session[:file_name] = @file_name
+  file = params[:file][:tempfile]
+  File.open("public/img/#{@file_name}", 'wb') do |f|
+    f.write(file.read)
+  end
+
   redirect "/articles/#{post_data.id}"
 end
