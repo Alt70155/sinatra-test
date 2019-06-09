f0215019fac6810ab475a72fa475e6282830f3d3
chika <chika1086@gmail.com>
2019-06-09 20:31:13 +0900

    Add process to limit the number of images in the article

--- helpers/article_img_valid?.rb
+++ helpers/article_img_valid?.rb
@@ -1,7 +1,8 @@
+require 'sinatra/reloader'
 helpers do
   def article_img_valid?(body, img_files)
     body_img_ct = body.scan(/!\[\S*\]\(\S*\)/).length
     ary = img_files.map { |img| !(img[:filename] !~ /.*\.(jpg|png|jpeg)\z/) }
-    body_img_ct == img_files.length && ary.all?
+    body_img_ct == img_files.length && img_files.length < 10 && ary.all?
   end
 end
